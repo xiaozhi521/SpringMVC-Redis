654b29cb978541436651e64f2634d58086d04d55
xiaozhi521 <1160948933@qq.com>
2018-05-12 21:26:19 +0800

    修订

--- SpringMVCTest/src/main/java/com/controller/HelloController.java
+++ SpringMVCTest/src/main/java/com/controller/HelloController.java
@@ -1,20 +1,54 @@
 package com.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.bean.Result;
+import com.util.ResultUtil;
+import jdk.nashorn.internal.runtime.JSONFunctions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 
-public class HelloController{
+public class HelloController {
 
-    @RequestMapping("/hello.do")
+    @RequestMapping("/hello.mvc")
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
-        model.addAttribute("ee","12121");
+        model.addAttribute("ee", "12121");
         return "hello";
     }
+
+    @RequestMapping("/getObj.mvc")
+    @ResponseBody
+    public Object s() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "test");
+        jsonObject.put("address", "bj");
+        Result result = new Result();
+        result.setCode(1);
+        result.setData(jsonObject);
+        result.setMessage("success");
+        return ResultUtil.returnObj(result);
+    }
+
+    @RequestMapping("/getMap.mvc")
+    @ResponseBody
+    public Map map() {
+        Map map = new HashMap();
+        map.put("name", "test");
+        map.put("address", "bj");
+        map.put("map", "this is map");
+        return map;
+    }
+
 
 }
--- SpringMVCTest/src/main/webapp/index.jsp
+++ SpringMVCTest/src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2><script language="javascript">
-    window.location = "/hello.do";
+    window.location = "/hello.mvc";
 </script></h2>
 </body>
 </html>
